685adc9bfaef1e98f01c5fe8a7e8799b21f23322
Tiago Amaral <tiagofly@outlook.com>
2025-09-06 02:01:06 -0300

    Update eventlistController.cs IMplementado rotas no controller.

--- src/modules/eventlist/infra/eventlistController.cs
+++ src/modules/eventlist/infra/eventlistController.cs
@@ -6,3 +6,37 @@
 * Copyright ©2024 Tiago Amaral. All rights reserved.
 */
 
+using System.Runtime.CompilerServices;
+using Microsoft.AspNetCore.Http.Features;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using Module.Eventlist.Storage.Entity;
+using Module.EventList.Service;
+using Module.EventList.Service.Interface;
+
+namespace Module.Eventlist.Infra.Controller;
+
+[Route("api/[controller]")]
+[ApiController]
+public class EventListController : ControllerBase
+{
+
+    private readonly IEventListServices eventListServices;
+
+    public EventListController(IEventListServices eventListServices)
+    {
+        this.eventListServices = eventListServices;
+    }
+
+    [HttpGet("FetchListEvents")]
+    public IActionResult GetAll() => Ok(this.eventListServices.GetAllAsync());
+
+    [HttpGet("FetchEventById")]
+    public IActionResult GetById(Guid id) => Ok(this.eventListServices.GetByIdAsync(id));
+
+    [HttpPost("CreateNewEvent")]
+    public IActionResult Create(EventModel data) => Ok(this.eventListServices.CreateAsync(data));
+
+    [HttpDelete("DeleteEventBytId")]
+    public IActionResult DeleteById(Guid id) => Ok(this.eventListServices.DeleteAsync(id));
+}
