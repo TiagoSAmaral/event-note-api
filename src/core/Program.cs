e20f6bdd6518bc22ca94b6884031af43882610a7
Tiago Amaral <tiagofly@outlook.com>
2025-09-06 02:01:23 -0300

    Update Program.cs Atualizado arquivo de boot para usar novos recursos.

--- src/core/Program.cs
+++ src/core/Program.cs
@@ -6,12 +6,29 @@
 * Copyright ©2024 Tiago Amaral. All rights reserved.
 */
 
+using Module.Eventlist.Infra.Controller;
+using Module.Eventlist.Service;
+using Module.Eventlist.Storage.Interface;
+using Module.EventList.Service.Interface;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Custom Module
+builder.Services.AddScoped<IEventListStorage, EventListStorage>();
+
+builder.Services.AddScoped<IEventListDeleteByIdentifierService, EventListDeleteByIdentifierService>();
+builder.Services.AddScoped<IEventListFetchByIdentifierService, EventListFetchByIdentifierService>();
+builder.Services.AddScoped<IEventListFetchService, EventListFetchService>();
+builder.Services.AddScoped<IEventListSaveService, EventListSaveService>();
+
+builder.Services.AddScoped<IEventListServices, EventListServices>();
+builder.Services.AddScoped<EventListController>();
 
 var app = builder.Build();
 
@@ -24,25 +41,25 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
+// var summaries = new[]
+// {
+//     "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+// };
 
-app.MapGet("/weatherforecast", () => {
-    var forecast =  Enumerable.Range(1, 5).Select(index => new WeatherForecast (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        )).ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast")
-.WithOpenApi();
+// app.MapGet("/weatherforecast", () => {
+//     var forecast =  Enumerable.Range(1, 5).Select(index => new WeatherForecast (
+//             DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+//             Random.Shared.Next(-20, 55),
+//             summaries[Random.Shared.Next(summaries.Length)]
+//         )).ToArray();
+//     return forecast;
+// })
+// .WithName("GetWeatherForecast")
+// .WithOpenApi();
 
 app.Run();
 
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
+// record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+// {
+//     public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+// }
