106d7bbd91b1da54b24b40f6bf969df5886545c2
Tiago Amaral <tiagofly@outlook.com>
2025-09-06 20:14:56 -0300

    Template de resposta padrao.

---
+++ src/shared/ResponseDefault/ResponseDefault.cs
@@ -0,0 +1,23 @@
+/*
+* ResponseDefault.cs 
+* event-list 
+*
+* Created by Tiago Amaral on 06/09/2025. 
+* Copyright ©2024 Tiago Amaral. All rights reserved.
+*/
+
+namespace event_list.shared.responsedefault;
+
+public class ResponseDefault
+{
+    public bool Status { get; set; }
+    public string Message { get; set; } = string.Empty;
+    public object? Data { get; set; }
+
+    public ResponseDefault(bool status, string message, object? data)
+    {
+        this.Status = status;
+        this.Message = message;
+        this.Data = data;
+    }
+}
