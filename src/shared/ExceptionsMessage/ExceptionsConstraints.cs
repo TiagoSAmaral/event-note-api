a504eac7c545d3d1a4233fe0d9677a897507b633
Tiago Amaral <tiagofly@outlook.com>
2025-09-06 20:14:00 -0300

    Implementado constraints de tamanhos para os campos.

---
+++ src/shared/ExceptionsMessage/ExceptionsConstraints.cs
@@ -0,0 +1,16 @@
+/*
+ * ExceptionsContraints.cs
+ * event-list
+ *
+ * Created by Tiago Amaral on 06/09/2025.
+ * Copyright ©2024 Tiago Amaral. All rights reserved.
+ */
+
+namespace event_list.shared.exceptionsMessage;
+
+public sealed class ExceptionsContraints
+{
+    public const int MaxSizeTitle = 100;
+    public const int MaxSizeDescription = 500;
+    public const int MaxSizeLocation = 200;
+}
