67a4e8669eeb040ecf137dc2ca17a6837491a083
Tiago Amaral <tiagofly@outlook.com>
2025-09-06 20:14:41 -0300

    Validador de data para o evento. Garantindo que valor seja sempre no futuro.

---
+++ src/shared/Dtos/FutureDateAttribute.cs
@@ -0,0 +1,23 @@
+/*
+* FutureDateAttribute.cs 
+* event-list 
+*
+* Created by Tiago Amaral on 06/09/2025. 
+* Copyright ©2024 Tiago Amaral. All rights reserved.
+*/
+
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace event_list.shared.dtos;
+
+public class FutureDateAttribute : ValidationAttribute
+{
+    public override bool IsValid(object? value)
+    {
+        if (value is not DateTime date)
+            return false;
+
+        return date > DateTime.UtcNow;
+    }
+}
