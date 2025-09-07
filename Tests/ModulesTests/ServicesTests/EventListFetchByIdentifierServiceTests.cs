ef706290f3ca37484b32a46bf384fc29f33e653e
Tiago Amaral <tiagofly@outlook.com>
2025-09-07 18:20:05 -0300

    Ajuste para executar testes corretamente no JB Rider. IMplementado Test para buscar evento por ID e exception caso o ID não seja encontrado.

---
+++ Tests/ModulesTests/ServicesTests/EventListFetchByIdentifierServiceTests.cs
@@ -0,0 +1,54 @@
+using event_list.modules.eventlist.services;
+using event_list.modules.eventlist.storage;
+using event_list.shared.exceptionsMessage;
+
+namespace event_list.Tests.ModulesTests.ServicesTests;
+
+using Xunit;
+using Moq;
+using FluentAssertions;
+
+public class EventListFetchByIdentifierServiceTests
+{
+    private readonly Mock<IEventListStorage> _mockStorage;
+    private readonly EventListFetchByIdentifierService _service;
+
+    public EventListFetchByIdentifierServiceTests()
+    {
+        _mockStorage = new Mock<IEventListStorage>();
+        _service = new EventListFetchByIdentifierService(_mockStorage.Object);
+    }
+
+    [Fact]
+    public async Task FetchEventListByIdentifier_ShouldReturnEvent_WhenFounded()
+    {
+        var expectedEvent = new EventFormDto();
+        var mockGuid = Guid.NewGuid();
+        expectedEvent.Id = mockGuid;
+        expectedEvent.Title = "Festa da Bananada!";
+        expectedEvent.Description = "Evento da cidade de Rio Bonito comemora o sucesso na producao e venda de bananada.";
+        expectedEvent.Date = DateTime.Parse("2026-09-07T18:37:08.531Z");
+        expectedEvent.Locale = "Rio Bonito/RJ";
+
+        _mockStorage
+            .Setup(request => request.GetByIdAsync(mockGuid))
+            .ReturnsAsync(expectedEvent);
+
+        var response = await _service.Fetch(mockGuid);
+        
+        response.Should().NotBeNull();
+        response.Title.Should().Be("Festa da Bananada!");
+    }
+    
+    [Fact]
+    public async Task FetchEventListByIdentifier_ShouldReturnNull_WhenIdIsInvalid()
+    {
+        var mockGuid = Guid.NewGuid();
+        _mockStorage.Setup(request => request.GetByIdAsync(mockGuid)).ReturnsAsync((EventFormDto?)null);
+        
+        Func<Task> action = async () => await _service.Fetch(mockGuid);
+        
+        // Assert
+        await action.Should().ThrowAsync<KeyNotFoundException>().WithMessage(ExceptionsMessages.EventNotFounded);
+    }
+}
