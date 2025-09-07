84ccfdc1ab6572ae6de0845d6da4a05a52c24f67
Tiago Amaral <tiagofly@outlook.com>
2025-09-07 00:35:47 -0300

    Ajuste de namespace. alteracao de tipo do status

--- src/shared/ResponseDefault/ResponseDefault.cs
+++ src/shared/ResponseDefault/ResponseDefault.cs
@@ -6,15 +6,15 @@
 * Copyright ©2024 Tiago Amaral. All rights reserved.
 */
 
-namespace event_list.shared.responsedefault;
+namespace event_list.shared.response_default;
 
 public class ResponseDefault
 {
-    public bool Status { get; set; }
+    public int Status { get; set; }
     public string Message { get; set; } = string.Empty;
     public object? Data { get; set; }
 
-    public ResponseDefault(bool status, string message, object? data)
+    public ResponseDefault(int status, string message, object? data)
     {
         this.Status = status;
         this.Message = message;
