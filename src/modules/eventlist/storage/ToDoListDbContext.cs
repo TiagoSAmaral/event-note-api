ae14aed63b0446b37fb95b9eaf9d90954c30dda8
Tiago Amaral <tiagofly@outlook.com>
2025-09-06 22:41:25 -0300

    Conector com banco de dados InMemory com validacao de campos antes de criar novo registro.

---
+++ src/modules/eventlist/storage/ToDoListDbContext.cs
@@ -0,0 +1,32 @@
+/*
+* ToDoListDbContext.cs 
+* event-list 
+*
+* Created by Tiago Amaral on 06/09/2025. 
+* Copyright ©2024 Tiago Amaral. All rights reserved.
+*/
+
+
+using Microsoft.EntityFrameworkCore;
+
+namespace event_list.modules.eventlist.storage;
+
+public class ToDoListDbContext : DbContext
+{
+    public DbSet<EventFormDto> Events => Set<EventFormDto>();
+    
+    public ToDoListDbContext(DbContextOptions<ToDoListDbContext> options) : base(options) {}
+    
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+
+        builder.Entity<EventFormDto>(ev =>
+        {
+            ev.HasKey(entity => entity.Id);
+            ev.Property(entity => entity.Title).IsRequired();
+            ev.Property( entity => entity.Description).IsRequired();
+            ev.Property( entity => entity.Locale).IsRequired();
+        });
+    }
+}
