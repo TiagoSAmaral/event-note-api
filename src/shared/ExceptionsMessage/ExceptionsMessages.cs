03ccb726b283b9ca239df2ca2fb102c830f63d32
Tiago Amaral <tiagofly@outlook.com>
2025-09-07 00:37:52 -0300

    Adicionado nova mensagem

--- src/shared/ExceptionsMessage/ExceptionsMessages.cs
+++ src/shared/ExceptionsMessage/ExceptionsMessages.cs
@@ -13,17 +13,19 @@ public sealed class ExceptionsMessages
 {
 
     public const string InvalidTitleMessage = "Nome inválido";
-    public const string MaxTitleSizeMessage = $"Titulo além do tamanho permitido";
+    public const string MaxTitleSizeMessage = "Titulo além do tamanho permitido";
 
     public const string InvalidDescriptionMessage = "Descrição inválida";
-    public const string MaxDescriptionSizeMessage = $"Descrição além do tamanho permitido";
+    public const string MaxDescriptionSizeMessage = "Descrição além do tamanho permitido";
 
     public const string InvalidDateMessage = "Data do evento inválida";
     public const string InvalidFutureDateMessage = "Data deve ser maior que atual";
 
     public const string InvalidLocationMessage = "Local inválido";
-    public const string MaxLocationSizeMessage = $"Local além do tamanho permitido";
+    public const string MaxLocationSizeMessage = "Local além do tamanho permitido";
 
     public const string SuccessCreateEvent = "Evento criado com sucesso";
     public const string SuccessDeleteEvent = "Evento apagado com sucesso";
+    
+    public const string EventNotFounded = "Evento não encontrado";
 }
