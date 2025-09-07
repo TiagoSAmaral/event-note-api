e3058d6e5495149cf7220676cb9e1acf85360869
Tiago Amaral <tiagofly@outlook.com>
2025-09-06 22:42:21 -0300

    Definido interface de métodos usado para services.

--- src/modules/eventlist/services/IEventListServices.cs
+++ src/modules/eventlist/services/IEventListServices.cs
@@ -6,13 +6,17 @@
 * Copyright ©2024 Tiago Amaral. All rights reserved.
 */
 
-using Module.Eventlist.Storage.Entity;
-namespace Module.EventList.Service.Interface;
+using event_list.shared.dtos;
+using event_list.modules.eventlist.storage;
 
+namespace event_list.modules.eventlist.services;
+/// <summary>
+/// Define a interface do modulo de Module.EventList.Service
+/// </summary>
 public interface IEventListServices 
 {
-    Task<IEnumerable<EventModel>> GetAllAsync();
-    Task<EventModel?> GetByIdAsync(Guid id);
-    Task CreateAsync(EventModel entity);
+    Task<IEnumerable<EventFormDto>> GetAllAsync();
+    Task<EventFormDto?> GetByIdAsync(Guid id);
+    Task CreateAsync(EventFormDto dto);
     Task DeleteAsync(Guid id);
 }
