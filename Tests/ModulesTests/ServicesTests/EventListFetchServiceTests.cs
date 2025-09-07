f305024218877878172a05a6622b2cf007f45c0e
Tiago Amaral <tiagofly@outlook.com>
2025-09-07 19:55:22 -0300

    Implementado teste para listar eventos.

---
+++ Tests/ModulesTests/ServicesTests/EventListFetchServiceTests.cs
@@ -0,0 +1,41 @@
+
+using event_list.modules.eventlist.services;
+using event_list.modules.eventlist.storage;
+using event_list.shared.exceptionsMessage;
+using event_list.Tests;
+using Xunit;
+using Moq;
+using FluentAssertions;
+
+namespace event_list.Tests.ModulesTests.ServicesTests;
+
+public class EventListFetchServiceTests
+{
+    private readonly Mock<IEventListStorage> _mockStorage;
+    private readonly EventListFetchService _service;
+    
+    public EventListFetchServiceTests()
+    {
+        _mockStorage = new Mock<IEventListStorage>();
+        _service = new EventListFetchService(_mockStorage.Object);
+    }
+
+    [Fact]
+    public async Task FetchEvents_WhenFounded()
+    {
+        // Arrange
+        var expectedEvents = new MockManager().GetEvents();
+
+        _mockStorage
+            .Setup(request => request.GetAllAsync())
+            .Returns(expectedEvents);
+        
+        // Act
+        var response = _service.Fetch();
+        
+        // Assert
+        response.Should().NotBeNull();
+        response.Count().Should().Be(expectedEvents.Count);
+        response.First().Id.Should().Be(expectedEvents.First().Id);
+    }
+}
