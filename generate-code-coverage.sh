57755d55de03cb6603d547972f4f8a5cf0187557
Tiago Amaral <tiagofly@outlook.com>
2025-09-08 03:31:25 -0300

    Script para execucao de testes unitarios e geracao de relatorio de cobertura de codigo

---
+++ generate-code-coverage.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Nome do script: generate-code-coverage.sh
+# Descrição: Realiza build, executa testes unitários do projeto e gera o relório de cobertura de código.
+# pré-requisito: Requer permissão de execução. Execute o comando: chmod +x generate-code-coverage.sh
+# Autor: Tiago Amaral
+# Data: 2025-09-08
+
+dotnet test --collect:"XPlat Code Coverage" && reportgenerator "-reports:**/TestResults/*/coverage.cobertura.xml" -targetdir:coveragereport && open coveragereport/index.html
