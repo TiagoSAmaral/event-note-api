8ef7b434da188837ec1fc1b5527b123d3edcd21d
Tiago Amaral <tiagofly@outlook.com>
2025-09-09 01:49:38 -0300

    Script de build e execucao do app

--- start-app-docker.sh
+++ start-app-docker.sh
@@ -1,6 +1,12 @@
-namespace event_list;
+#!/bin/bash
+# Nome do script: start-app-docker.sh
+# Descrição: Realiza build, executa o projeto no docker.
+# pré-requisito: Requer permissão de execução. Execute o comando: chmod +x start-app-docker.sh
+# Autor: Tiago Amaral
+# Data: 2025-09-08
 
-public class start_app_docker_sh
-{
-    
-}
+# Criar a imagem
+docker build -t event-list-api .
+
+# Rodar a imagem em produção
+docker run -d -p 5185:5185 --name event-list-api event-list-api
